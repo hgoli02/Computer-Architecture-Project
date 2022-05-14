be086002416571aa51907be93fd4b48f6b79b1fa
alirf2077 <alirf2077@gmail.com>
2022-05-15 01:04:18 +0430

    alu and alu controller changed

--- src/alu.sv
+++ src/alu.sv
@@ -7,12 +7,107 @@ module ALU #(
     out,
     negative,
     zero
+    //overflow
 );
     
     input[XLEN-1:0] input1,input2;
     input[3:0] alu_operation; //TODO check size
-    output[XLEN-1:0] out;
+    output reg [XLEN-1:0] out;
     output zero;
     output negative;
+    //output overflow;
+
+    localparam [3:0] NOP = 4'd0, XOR = 4'd1, OR = 4'd2, AND = 4'd3,
+                     NOR = 4'd4 ,SLL = 4'd5, SRL = 4'd6, SLT = 4'd7, ADD = 4'd8,
+                     ADDU = 4'd9, SUB = 4'd10, SUBU = 4'd11, MULT = 4'd12, DIV = 4'd13 ;
+
+    assign zero = ~|out;
+
+    assign negative = out[XLEN - 1];
+
+
+    always @(input1, input2, alu_operation) begin
+        case(alu_operation)
+            XOR : out = input1 ^ input2;
+
+            OR :  out = input1 | input2;
+
+            AND :  out = input1 & input2;
+
+            NOR :  out = input1 ~| input2;
+
+            SLL : out = input1 << input2;
+
+            SRL : out = input1 >> input2;
+
+            ADD : out = input1 + input2;
+
+            ADDU : out = input1 + input2;
+
+            SUB : out = input1 - input2;
+
+            SUBU : out = input1 - input2;
+            
+            MULT : out = input1 * input2;
+
+            DIV : out = input1 / input2;
+
+             SLT : 
+                begin
+                out = input1 - input2;
+                if(out[31])
+                out = 1;
+                else
+                out = 0;
+                end
+            default : out = 32'b0; //TODO : 32 to be replaced with XLEN
+        endcase
+    end
+
+
+
+
+
+
+
+
+
+
+
+    // assign out = alu_operation == 4'b0001 ? input1 ^ input2 : XLEN'bz; 
+    
+    // assign out = alu_operation == 4'b0010 ? input1 | input2 : XLEN'bz;
+    
+    // assign out = alu_operation == 4'b0011 ? input1 & input2 : XLEN'bz;
+    
+    // assign out = alu_operation == 4'b0100 ? input1 ~| input2 : XLEN'bz;
+    
+    // assign out = alu_operation == 4'b0101 ? input1 << input2 : XLEN'bz;
+    
+    // assign out = alu_operation == 4'b0110 ? input1 >> input2 : XLEN'bz;
+    
+    // assign out = alu_operation == 4'b0111 ? input1 > input2 : XLEN'bz; // he'b
+    
+    // assign out = alu_operation == 4'b1000 ? input1 + input2 : XLEN'bz;
+    
+    // assign out = alu_operation == 4'b1001 ? input1 + input2 : XLEN'bz;
+
+    // assign out = alu_operation == 4'b1010 ? input1 - input2 : XLEN'bz;
+    
+    // assign out = alu_operation == 4'b1011 ? input1 - input2 : XLEN'bz;
+
+    // assign out = alu_operation == 4'b1100 ? input1 * input2 : XLEN'bz;
+
+    // assign out = alu_operation == 4'b1101 ? input1 \ input2 : XLEN'bz;
+
+    // assign out = alu_operation == 4'b0000 ? input1 : XLEN'bz;
+
+    // assign zero = ~|out;
+
+    // assign negative =  ?  : XLEN'bz
+
+
+
+
 
 endmodule
--- src/alu_controller.sv
+++ src/alu_controller.sv
@@ -6,47 +6,53 @@ module ALU_CONTROLLER(alu_operation, opcode, func, clk);
     input [5:0] func;
     input clk;
 
+    localparam [3:0] NOP = 4'd0, XOR = 4'd1, OR = 4'd2, AND = 4'd3,
+                     NOR = 4'd4 ,SLL = 4'd5, SRL = 4'd6, SLT = 4'd7, ADD = 4'd8,
+                     ADDU = 4'd9, SUB = 4'd10, SUBU = 4'd11, MULT = 4'd12, DIV = 4'd13 ;
+
+    always @(*) begin
+        
 
     // R instructions
     if (opcode == 6'b000000) begin
-        if(func == 6'b000000 || func == 6'b000100) //SLL, SLLV
-            alu_operation = 4'd5;
+        if(func == 6'b000100) //SLL, SLLV
+            alu_operation = SLL;
 
-        if(func == 6'b000010 || func == 6'b000110) //SRL, SRLV
-            alu_operation = 4'd6;
+        if(func == 6'b000110) //SRL, SRLV
+            alu_operation = SRL;
 
         if(func == 6'b100110) //xor
-            alu_operation = 4'd1;
+            alu_operation = XOR;
 
         if(func == 6'b100010) // sub
-            alu_operation = 4'd10;
+            alu_operation = SUB;
 
         if(func == 6'b101010) // slt
-            alu_operation = 4'd7;
+            alu_operation = SLT;
 
         if(func == 6'b100011) //sub unsigned
-            alu_operation = 4'd11;
+            alu_operation = SUBU;
 
         if(func == 6'b100101) // OR
-            alu_operation = 4'd2;
+            alu_operation = OR;
 
         if(func == 6'b100111) //NOR
-            alu_operation = 4'd4;
+            alu_operation = NOR;
 
         if(func == 6'b100001) //add unsigned
-            alu_operation = 4'd8;
+            alu_operation = ADDU;
 
         if(func == 6'b011000) //mult
-            alu_operation = 4'd12;
+            alu_operation = MULT;
 
         if(func == 6'b011010) //div
-            alu_operation = 4'd13;
+            alu_operation = DIV;
 
         if(func == 6'b100100) //AND
-            alu_operation = 4'd3;
+            alu_operation = AND;
 
         if(func == 6'b100000) //add
-            alu_operation = 4'd9;
+            alu_operation = ADD;
 
         
     end
@@ -54,21 +60,39 @@ module ALU_CONTROLLER(alu_operation, opcode, func, clk);
     // I instructions
     else begin
         if(opcode == 6'b001110) //XORi
-            alu_operation = 4'd1;
+            alu_operation = XOR;
 
         if(opcode == 6'b001010) //SLTi
-            alu_operation = 4'd7;
+            alu_operation = SLT;
 
         if(opcode == 6'b001000) //ADDi
-            alu_operation = 4'd8;
+            alu_operation = ADD;
 
         if(opcode == 6'b001100) //ANDi
-            alu_operation = 4'd3;
+            alu_operation = AND;
 
         if(opcode == 6'b001101) //ORi 
-            alu_operation = 4'd2;   
+            alu_operation = OR;   
 
-         if(opcode == 6'b001001) //ADDiu (unsigned) 
-            alu_operation = 4'd8;   
+        if(opcode == 6'b001001) //ADDiu (unsigned) 
+            alu_operation = ADDU;
+
+        if(opcode == 6'b000100) //BEQ 
+            alu_operation = SUB;
+
+        if(opcode == 6'b000101) //BNE
+            alu_operation = SUB;
+
+        if(opcode == 6'b000110) //BLEZ
+            alu_operation = NOP;
+
+        if(opcode == 6'b000111) //BGTZ
+            alu_operation = NOP;
+
+        if(opcode == 6'b000001) //BGEZ 
+            alu_operation = NOP;
+        
+        
+        end
         end
 endmodule
