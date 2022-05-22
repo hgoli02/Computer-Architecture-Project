d07e0ba8c911f989b99c72825d579d4a7f5f9c7e
erfansadraiyeh <sadraiyeh.e2002@gmail.com>
2022-05-22 17:24:26 +0430

    slt test added.

---
+++ test/custom/slt.s
@@ -0,0 +1,15 @@
+	.text
+main:   
+        addi $v0,$zero,10
+        addi $t0,$zero,5
+        addi $t1,$zero,-3
+        slt $t2,$t0,$t1
+        slt $t3,$t1,$t0
+        slti $t4,$t1,2
+        slti $t5,$t1,-5
+        slti $t6,$t0,2
+        slti $t7,$t0,10
+        add $t1,$zero,$t0
+        slt $t8,$t1,$t0
+        slti $t9,$zero,0
+        syscall
