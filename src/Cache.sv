26a20e70fa7b329bd45ad7855fa99e1ba939a7ea
hgoli02 <hosseingoli8899@gmail.com>
2022-06-04 11:54:25 +0430

    cache interface addedd

--- src/Cache.sv
+++ src/Cache.sv
@@ -1,5 +1,15 @@
 module cache (
-    
+    addr, data_in, data_out, we, clk, rst_b   
 );
-    
+    parameter XLEN = 32;
+    parameter C_WIDTH = 10;//random 10 neveshatm dorost she
+
+    output [XLEN - 1 : 0] data_out;
+
+    input [XLEN - 1 : 0] data_in;
+    input [C_WIDTH - 1: 0] addr;
+    input we;
+    input clk;
+    input rst_b;
+
 endmodule
