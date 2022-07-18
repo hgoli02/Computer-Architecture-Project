918ae9dde407d5132f3a26b47783d8c2bbc33d4b
amrossein <amirbar2014@gmail.com>
2022-07-18 18:26:38 +0430

    null

--- src/data_path.sv
+++ src/data_path.sv
@@ -425,8 +425,10 @@ always @(negedge clk) begin
     $display("\n\n*************************************\nclock  %d\n\n",x);
     $display("fs_data_EX = %h, ft_data_EX = %h, alu_operation_EX = %d, fp_alu_result_EX = %h",fs_data_EX ,ft_data_EX, alu_operation_EX, fp_alu_result_EX);
     $display("rt_data ID, EX, MEM, WB = %h %h %h %h", rt_data_ID, rt_data_EX, rt_data_MEM, rt_data_WB);
-    $display("fs_data = %h, ft_data = %h, fs_num = %h, ft_num = %h", fs_data_ID, ft_data_ID, inst_ID[25:21], inst_ID[20:16]);
+    $display("fs_data = %h, ft_data = %h, fs_num = %h, ft_num = %h, fd_num = %h", fs_data_ID, ft_data_ID, inst_ID[25:21], inst_ID[20:16], rd_num_WB);
     $display("fd_data = %h", fd_data);
+    $display("regfile_mux ID, EX, MEM, WB: %h %h %h %h", regfile_mux, regfile_mux_EX, regfile_mux_MEM, regfile_mux_WB);
+    $display("fs_data ID, EX, MEM, WB = %h %h %h %h", fs_data_ID, fs_data_EX, fs_data_MEM, fs_data_WB);
     $display("func ID = %h, opcode_ID = %h", inst_ID[5 : 0], inst_ID[31 : 26]);
     $display("fp_regfile_mux ID, EX, MEM, WB = %h %h %h %h", fp_regfile_mux, fp_regfile_mux_EX, fp_regfile_mux_MEM, fp_alu_result_WB);
     $display("float_reg_write_enable ID, EX, MEM, WB = %h %h %h %h", float_reg_write_enable, float_reg_write_enable_EX, float_reg_write_enable_MEM, float_reg_write_enable_WB);
